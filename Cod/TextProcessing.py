2b95f5f2deac4a21c8c0da012d609a0a0402d44c
Liutalie <62174353+Liutalie@users.noreply.github.com>
2024-05-16 00:39:43 +0300

    Strarted implementation of question classification

--- Cod/TextProcessing.py
+++ Cod/TextProcessing.py
@@ -3,9 +3,8 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.util import ngrams
 from sklearn.feature_extraction.text import TfidfVectorizer
 import spacy
-
-
-# from deep_translator import GoogleTranslator
+from nltk.corpus import wordnet
+from deep_translator import GoogleTranslator
 
 # def translateText(self):
 #     with open('Dataset/Test.txt', 'r') as file:
@@ -16,7 +15,104 @@ import spacy
 class TextProcessing:
     def __init__(self):
         self.m_stopWords = []
-        self.nlp = spacy.load('ro_core_news_sm')
+        self.nlp = spacy.load('en_core_web_sm')
+        self.datasets = {}
+
+    def readData(self):
+        trainData = {}
+        testData = {}
+
+        f1 = open("TREC/train_data.txt")
+        f2 = open("TREC/test_data.txt")
+
+        for line in f1:
+            firstSplit = line.split(" ", 1)
+            classes = firstSplit[0]
+            classesSplit = classes.split(":")
+            question = firstSplit[1]
+            trainData[question.rstrip("\n")] = classesSplit
+
+        for line in f2:
+            classes = line.split(" ", 1)[0]
+            classesCourse = classes.split(":")[0]
+            classesFine = classes.split(":")[1]
+            question = line.split(" ", 1)[1]
+            testData[question.rstrip("\n")] = [classesCourse, classesFine]
+
+        self.datasets['train'] = trainData
+        self.datasets['test'] = testData
+
+    def extractHeadWord(self, query):
+        listOfDependencies = {}
+        headword = ''
+        for token in query:
+            # build the dependency list
+            listOfDependencies[token.dep_] = token
+
+        if ("nsubj" in listOfDependencies) or (
+                "nsubjpass" in listOfDependencies):  # if the question has a subject
+            for token in query:
+                if token.dep_ in ["nsubj", "nsubjpass"]:
+                    if token.pos_ in ["NOUN", "ADV",
+                                      "PROPN"]:  # if the subj is a noun or adverb, pick it as the headword
+                        headword = token.lemma_.lower()
+                        break
+                    if token.pos_ == "PRON":
+                        for token2 in query:
+                            if token2.dep_ == "dobj" and token2.pos_ in ["NOUN",
+                                                                         "ADV"]:  # if the subj is a pronoun, get the direct object
+                                headword = token2.lemma_.lower()
+                                break
+                            if token2.dep_ == "dobj" and token2.pos_ == "PROPN":
+                                headword = "name"
+        elif "attr" in listOfDependencies:
+            for token in query:
+                if token.dep_ == "attr" and (token.pos_ in ["NOUN", "ADV", "PROPN"]):
+                    headword = token.lemma_.lower()
+
+        else:  # if the question has no subject, consider it to be a "Name x" type of question and get the direct obj
+            for token in query:
+                if token.dep_ == "dobj" and token.pos_ in ["NOUN", "ADV"]:
+                    headword = token.lemma_.lower()
+                    break
+
+        if headword == "":  # if no headword has been found, pick the first noun as the headword
+            for token in query:
+                if token.pos_ in ["NOUN", "PROPN"]:
+                    headword = token.lemma_.lower()
+                    break
+
+        return headword
+
+    def extractHypernim(self, headword):
+        hypernim = '-'
+        if len(wordnet.synsets(headword)) > 0:
+            syn = wordnet.synsets(headword)[0]
+            hypernim = syn.hypernym_paths()[0]
+        return hypernim
+
+    def hypernimUntilRoot(self, hypernim):
+        if len(hypernim) > 6:
+            hypernym = hypernim[6]  # to be checked
+            hypernym = str(hypernym).split(".")[0]
+            hypernym = hypernym.split("'")[1]
+        else:
+            hypernym = hypernim[len(hypernim) - 1]  # to be checked
+            hypernym = str(hypernym).split(".")[0]
+            hypernym = hypernym.split("'")[1]
+        return hypernym
+
+    def queryExpansion(self, hypernim):
+        queryExpansion = []
+        if len(hypernim) > 0:
+            hypWeight = 1
+            for hyp in reversed(hypernim):
+                hyp = str(hyp).split(".")[0]
+                print(hyp)
+                hyp = hyp.split("'")[1]
+                queryExpansion.append(hyp + "~" + str(hypWeight))
+                hypWeight = round(hypWeight * 60 / 100, 2)
+        return queryExpansion
 
     def readStopWords(self):
         with open('stop_words_romanian.json', 'r', encoding='utf-8') as file:
@@ -42,8 +138,8 @@ class TextProcessing:
         return lemmatized_words
 
     def nGrams(self, query):
-        unigram_list = list(ngrams(query.split(), 1))
-        bigram_list = list(ngrams(query.split(), 2))
+        unigram_list = list(ngrams(query, 1))
+        bigram_list = list(ngrams(query, 2))
         return unigram_list, bigram_list
 
     def partOfSpeech(self, query):
--- Cod/main.py
+++ Cod/main.py
@@ -1,31 +1,153 @@
+import pandas
+import sklearn
+import numpy as np
+from sklearn.svm import LinearSVC
+
 import TextProcessing
+
+tempX = None
+tempY = None
+
+def trainAlgorithm(training_feature_list, true_positive_training, SVM):
+    for list in training_feature_list:
+        newMatrix = []
+        all_features = []
+        for feature_list in training_feature_list:
+            for elem in feature_list:
+                all_features.append(elem)
+
+        uniqueFeatures = list(set(all_features))
+        rowTemplate = dict.fromkeys(uniqueFeatures, 0)
+
+        for feature_list in training_feature_list:
+            row = rowTemplate.copy()
+            for elem in feature_list:
+                row[elem] = 1
+
+            newMatrix.append(row)
+        myDataframe = pandas.DataFrame(newMatrix)
+
+    X_train1, X_test1, y_train1, y_test1 = sklearn.model_selection.train_test_split(myDataframe,
+                                                                                    true_positive_training, test_size=0.2,
+                                                                                    shuffle=False)
+
+    global tempX
+    global tempY
+    tempX = X_test1
+    tempY = y_test1
+    myDataframe = X_train1
+    true_positive_training = y_train1
+    SVM.fit(myDataframe, true_positive_training)
+
+def testAlgorithm(testing_feature_list, true_positive_testing, SVM):
+    global tempX
+    global tempY
+    for list in training_feature_list:
+        newMatrix = []
+        all_features = []
+        for feature_list in training_feature_list:
+            for elem in feature_list:
+                all_features.append(elem)
+
+        uniqueFeatures = list(set(all_features))
+        rowTemplate = dict.fromkeys(uniqueFeatures, 0)
+
+        for feature_list in training_feature_list:
+            row = rowTemplate.copy()
+            for elem in feature_list:
+                row[elem] = 1
+
+            newMatrix.append(row)
+        myDataframe = pandas.DataFrame(newMatrix)
+
+    results= SVM.predict(myDataframe)
+
+    testData = sklearn.metrics.classification_report(results, true_positive_testing,
+                                                              zero_division=0,labels=np.unique(results))
+    pass
+
 
 if __name__ == '__main__':
     textProcessing = TextProcessing.TextProcessing()
-    textProcessing.readStopWords()
-    # textProcessing.translateText()
-    query = "Acesta este un exemplu de query"
-    query = query.lower()
+    textProcessing.readData()
 
-    queryStemming = textProcessing.stemmingOfWords(query)
-    print(queryStemming)
+    training_feature_list = []
+    testing_feature_list = []
+    true_positive_training = []
+    true_positive_testing = []
+    SVM = LinearSVC()
 
-    queryLemma = textProcessing.lemmaOfWords(query)
-    print(queryLemma)
 
-    queryStopWords = textProcessing.removeStopWords(query)
-    print(queryStopWords)
+    for dataset in textProcessing.datasets.items():
+        for key in dataset[1]:
+            question = key
+            category = dataset[1][key][1]
 
-    queryUnigram, queryBigram = textProcessing.nGrams(query)
-    print(queryUnigram)
-    print(queryBigram)
+            doc = textProcessing.nlp(question)
+            # question features
+            unigrams = []
+            bigrams = []
+            wordShapes = []
+            headword = ''
+            hypernym = '-'
+            relatedWords = []
+            queryExpansion = []
+            questionCategory = ''
 
-    part_of_speech = textProcessing.partOfSpeech(query)
-    print(part_of_speech)
+            wordShapeDict = {"lower": 0, "mixed": 0, "numbers": 0, 'other': 0}
 
-    dependencies = textProcessing.dependencyParsing(query)
-    print(dependencies)
+            unigrams, bigrams = textProcessing.nGrams(doc)
+            headword = textProcessing.extractHeadWord(doc)
+            if headword == '':  # could not find the headword
+                headword = '-'
+            else:
+                hypernym = textProcessing.extractHypernim(headword)
+                queryExpansion = textProcessing.queryExpansion(hypernym)
+                hypernym = textProcessing.hypernimUntilRoot(hypernym)
+            list_of_feature = [unigrams, bigrams, headword, hypernym, queryExpansion]
+            unpacked_features = []
+            for item in list_of_feature:
+                if isinstance(item, list):
+                    unpacked_features.extend(item)
+                else:
+                    unpacked_features.append(item)
 
-    keywords, matrix = textProcessing.keywordExtraction(query)
-    print(matrix)
-    print(keywords)
+            if dataset[0] == 'train':
+                training_feature_list.append(unpacked_features)
+                true_positive_training.append(category)
+            else:
+                testing_feature_list.append(unpacked_features)
+                true_positive_testing.append(category)
+    trainAlgorithm(training_feature_list,true_positive_training,SVM)
+    testAlgorithm(training_feature_list,true_positive_training,SVM)
+
+
+
+
+    # textProcessing.readStopWords()
+    # # textProcessing.translateText()
+    # query = "Acesta este un exemplu de query"
+    # query = query.lower()
+    #
+    # queryStemming = textProcessing.stemmingOfWords(query)
+    # print(queryStemming)
+    #
+    # queryLemma = textProcessing.lemmaOfWords(query)
+    # print(queryLemma)
+    #
+    # queryStopWords = textProcessing.removeStopWords(query)
+    # print(queryStopWords)
+    #
+    # queryUnigram, queryBigram = textProcessing.nGrams(query)
+    # print(queryUnigram)
+    # print(queryBigram)
+    #
+    # part_of_speech = textProcessing.partOfSpeech(query)
+    # print(part_of_speech)
+    #
+    # dependencies = textProcessing.dependencyParsing(query)
+    # print(dependencies)
+    #
+    # keywords, matrix = textProcessing.keywordExtraction(query)
+    # print(matrix)
+    # print(keywords)
